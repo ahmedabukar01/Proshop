1cef36db610da519ed017eca4a5e24868fb0a20b
ahmed <ahmedlearn01@gmail.com>
2022-05-11 06:53:27 -0700

    head & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <main>
       <h2>Proshop</h2>
+      </main>
+      <Footer />
     </div>
   );
 }
