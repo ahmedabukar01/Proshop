22e19a1834439d12706edca2aaf6a673921e9817
ahmed <ahmedlearn01@gmail.com>
2022-05-11 07:26:57 -0700

    fontawasome & bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h2>Proshop</h2>
         </Container>
