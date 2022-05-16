b4becd7f6930b6c6a394c16588cde6849fe76cc6
ahmed <ahmedlearn01@gmail.com>
2022-05-16 09:44:37 -0700

    product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
+import { Row, Col, Button, Card } from 'react-bootstrap';
+import products from '../products';
+import Rating from '../components/Rating';
 
 const ProductScreen = () => {
   return (
