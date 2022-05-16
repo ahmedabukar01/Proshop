35754a32a8aec465abc79b60af76c6cb9f1080d7
ahmed <ahmedlearn01@gmail.com>
2022-05-16 10:08:05 -0700

    initial commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -23,6 +23,12 @@ const ProductScreen = () => {
                     <ListGroup.Item>
                         <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
                     </ListGroup.Item>
+                    <ListGroup.Item>
+                        Price: ${product.price}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Description: ${product.description}
+                    </ListGroup.Item>
                 </ListGroup>
             </Col>
         </Row>
