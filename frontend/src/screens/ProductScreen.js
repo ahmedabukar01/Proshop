9436abf60390c855aacf9a1e27f9f047fe4fafe8
ahmed <ahmedlearn01@gmail.com>
2022-05-16 10:05:51 -0700

    list group

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import {Link, useParams} from 'react-router-dom';
-import { Row, Col, Button, Card } from 'react-bootstrap';
+import { Row, Col, Button, Card, ListGroup, Image} from 'react-bootstrap';
 import products from '../products';
 import Rating from '../components/Rating';
 
@@ -7,7 +7,26 @@ const ProductScreen = () => {
     const params = useParams();
     const product = products.find(product => product._id === params.id)
   return (
-    <div>{product.name}</div>
+    <>
+        <Link className='btn btn-light my-3' to="/">
+            Go Back
+        </Link>
+        <Row>
+            <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid/>
+            </Col>
+            <Col md={3}>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>
+                        <h2>{product.name}</h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+        </Row>
+    </>
   )
 }
 
