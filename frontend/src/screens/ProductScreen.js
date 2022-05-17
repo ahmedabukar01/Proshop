b267ecbd8b9ef4fb4fd4135828d05641866eff63
ahmed <ahmedlearn01@gmail.com>
2022-05-16 17:37:33 -0700

    details product

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -31,6 +31,33 @@ const ProductScreen = () => {
                     </ListGroup.Item>
                 </ListGroup>
             </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Price: </Col>
+                                <Col>
+                                    <strong>${product.price}</strong>
+                                </Col>
+
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Status: </Col>
+                                <Col>
+                                    {product.countInStock > 0? "In Stock" : "out of Stock"}
+                                </Col>
+
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button className='btn-block' type="button" disabled={product.countInStock === 0}>Add To Cart</Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
         </Row>
     </>
   )
