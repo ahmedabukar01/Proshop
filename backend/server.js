e5e530f7c76bbc1e490f79f335432bf120e49816
ahmed <ahmedlearn01@gmail.com>
2022-05-21 10:45:10 -0700

    fixed

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,6 @@ app.get('/api/products/:id',(req,res)=>{
 const PORT = process.env.PORT || 5000
 app.listen(
     PORT, 
-    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    .yellow.bold)
     )
