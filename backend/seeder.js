e3a0bbb09afbc80a5ee2a4e85e595729fa084abe
ahmed <ahmedlearn01@gmail.com>
2022-05-22 09:48:23 -0700

    data import  ddestroy

--- backend/seeder.js
+++ backend/seeder.js
@@ -6,3 +6,55 @@ import products from './data/products.js'
 import User from './models/userModel.js'
 import Product from './models/ProductModel.js'
 import Order from './models/ordersModel.js'
+import connectDB from './config/db.js'
+
+detenv.config()
+
+connectDB();
+
+const importData = ()=>{
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        const createdUsers = await User.insertMany(users);
+        const adminUser = createdUsers[0]._id;
+
+        const sampleProducts = products.map(product=>{
+            return {...product, user: adminUser}
+        })
+
+        await Product.insertMany(sampleProducts);
+
+        console.log('Data Imported!'.green.inverse);
+
+        process.exit(1);
+
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1)
+    }
+}
+
+const destroyData = ()=>{
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        console.log('Data Destroyed!'.red.inverse);
+
+        process.exit(1);
+
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1)
+    }
+}
+
+if(process.argv[2] === '-d'){
+    destroyData();
+} else{
+    importData();
+}
