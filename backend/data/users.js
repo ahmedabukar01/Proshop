c957636bd1a85ea8ba09862ad8cb6206d6051984
ahmed <ahmedlearn01@gmail.com>
2022-05-22 09:19:08 -0700

    user

--- backend/data/users.js
+++ backend/data/users.js
@@ -8,12 +8,12 @@ const users = [
         email: 'admin@example.com',
         password: bcrypt.hashSync('123456',10),
         isAdmin: true
-    }
+    },
     {
         name: 'john Doe',
         email: 'john@example.com',
         password: bcrypt.hashSync('123456',10),
-    }
+    },
     {
         name: 'jane Doe',
         email: 'jane@example.com',
