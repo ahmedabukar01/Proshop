8c200a3c263ff52d9f4f57346ffa9dc9a7b71061
ahmed <ahmedlearn01@gmail.com>
2022-06-19 14:43:09 -0700

    err middleware is runing

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,15 @@ app.get('/',(req,res)=>{
     res.send('Api is Running')
 })
 
+app.use((err, req,res,next) =>{
+    const statusCode = res.statusCode ===200 ? 500 : res.statusCode
+    res.status(statusCode);
+    res.json({
+        message: err.massage,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+})
+
 app.use('/api/products',productRoutes);
 
 const PORT = process.env.PORT || 5000
