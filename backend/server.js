d539a41fc7436c3dbc7b5956e3b3f1b8b60e182f
ahmed <ahmedlearn01@gmail.com>
2022-06-19 15:12:45 -0700

    error middle wares is up & runnign

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,15 @@ app.get('/',(req,res)=>{
     res.send('Api is Running')
 })
 
+app.use((req,res,next)=>{
+    const error = new Error(`not found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+})
+
 app.use((err, req,res,next) =>{
-    const statusCode = res.statusCode ===200 ? 500 : res.statusCode
-    res.status(statusCode);
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode);    
     res.json({
         message: err.massage,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
